def5f1ee43716dde5e7dd78f775a3c7e260ad9c2
Danielle <xdanielle.dsouza@gmail.com>
2020-12-12 17:20:08 -0500

    Responsive fixes

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../../styles/_forms.scss';
 import { connect } from 'react-redux';
 import { signUp } from '../../store/actions/authActions';
 
@@ -29,8 +30,8 @@ class SignUp extends Component {
 
         return (
             <div className="container">
-                <form className="white" onSubmit={ this.handleSubmit }>
-                    <h5 className="grey-text text-darken-3">Sign Up</h5>
+                <form onSubmit={ this.handleSubmit }>
+                    <h5>Sign Up</h5>
                     <div className="input-field">
                         <label htmlFor="firstName">First Name</label>
                         <input type="text" id="firstName" onChange={ this.handleChange } />
@@ -48,7 +49,7 @@ class SignUp extends Component {
                         <input type="password" id="password" onChange={ this.handleChange } />
                     </div>
                     <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+                        <button className="btn z-depth-0">Sign Up</button>
                         <div className="center red-text">
                             { authError ? <p>{ authError }</p> : null}
                         </div>
--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -13,7 +13,7 @@ export default class Home extends Component {
           <hr />
           <div className="quick-notes">
             <div>
-              <i class="material-icons medium">public</i>
+              <i className="material-icons medium">public</i>
               <h3>Track your travels!</h3>
               <p>
                 Track your plan for world domination by documenting your
@@ -21,12 +21,12 @@ export default class Home extends Component {
               </p>
             </div>
             <div>
-            <i class="material-icons medium">person</i>
+            <i className="material-icons medium">person</i>
               <h3>Connect with friends!</h3>
               <p>Share your adventures with friends!</p>
             </div>
             <div>
-            <i class="material-icons medium">location_on</i>
+            <i className="material-icons medium">location_on</i>
               <h3>Research your next trip!</h3>
               <p>Read about others' experiences in different countries!</p>
             </div>
