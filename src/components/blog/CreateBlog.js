a788d6cf5404c4c7b86c390d6dedf7103bc185c9
Danielle <xdanielle.dsouza@gmail.com>
2020-12-12 15:19:12 -0500

    Form styles

--- src/components/blog/CreateBlog.js
+++ src/components/blog/CreateBlog.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-//import { withFirebase } from 'react-redux-firebase';
-import { createBlog } from '../../store/actions/blogActions';
+import '../../styles/_forms.scss';
+import { connect } from "react-redux";
+import { createBlog } from "../../store/actions/blogActions";
 
 class CreateBlog extends Component {
-
   state = {
     title: "",
     content: "",
@@ -19,24 +18,28 @@ class CreateBlog extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.createBlog(this.state);
-    this.props.history.push('/');
+    this.props.history.push("/");
   };
 
   render() {
     return (
       <div className="container">
-        <form className="white" onSubmit={this.handleSubmit}>
-          <h5 className="grey-text text-darken-3">Create Blog</h5>
+        <form onSubmit={this.handleSubmit}>
+          <h5>Create Blog</h5>
           <div className="input-field">
             <label htmlFor="title">Title</label>
             <input type="text" id="title" onChange={this.handleChange} />
           </div>
           <div className="input-field">
             <label htmlFor="content">Content</label>
-            <textarea className="materialize-textarea" id="content" onChange={ this.handleChange }></textarea>
+            <textarea
+              className="materialize-textarea"
+              id="content"
+              onChange={this.handleChange}
+            ></textarea>
           </div>
           <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Post</button>
+            <button className="btn z-depth-0">Post</button>
           </div>
         </form>
       </div>
@@ -46,8 +49,8 @@ class CreateBlog extends Component {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    createBlog: (blog) => dispatch(createBlog(blog))
-  }
-}
+    createBlog: (blog) => dispatch(createBlog(blog)),
+  };
+};
 
 export default connect(null, mapDispatchToProps)(CreateBlog);
